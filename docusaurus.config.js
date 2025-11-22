a4884736f21cfc3babb6e3bf4558560810c74b58
freemeing <aw565@duke.edu>
2025-11-22 11:20:07 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Wow! Maker Journey',
+  title: 'Wow! Maker Jou',
   tagline: 'Everything Infinite ',
   favicon: 'img/favicon.ico',
 
